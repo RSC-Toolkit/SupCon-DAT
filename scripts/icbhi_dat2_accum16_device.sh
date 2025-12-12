c9507f53a4809c2165c662f13c161d9aea3d72cf
kaen2891 <kaen2891@gmail.com>
2025-12-12 21:51:02 +0000

    added

---
+++ scripts/icbhi_dat2_accum16_device.sh
@@ -0,0 +1,48 @@
+
+MODEL="ast"
+SEED="1 2 3 4 5"
+
+for s in $SEED
+do
+    for m in $MODEL
+    do
+        TAG="bs8_lr5e-5_ep50_seed${s}_dat2_dev"
+        CUDA_VISIBLE_DEVICES=0 python main.py --tag $TAG \
+                                        --dataset icbhi \
+                                        --seed $s \
+                                        --class_split wheeze \
+                                        --n_cls 2 \
+                                        --epochs 50 \
+                                        --batch_size 8 \
+                                        --optimizer adam \
+                                        --learning_rate 5e-5 \
+                                        --weight_decay 1e-6 \
+                                        --cosine \
+                                        --model $m \
+                                        --test_fold official \
+                                        --pad_types repeat \
+                                        --weighted_loss \
+                                        --resz 1 \
+                                        --n_mels 128 \
+                                        --ma_update \
+                                        --ma_beta 0.5 \
+                                        --from_sl_official \
+                                        --audioset_pretrained \
+                                        --method ce \
+                                        --domain_adaptation2 \
+                                        --alpha 1.0 \
+                                        --proj_dim 768 \
+                                        --temperature 0.06 \
+                                        --meta_mode dev \
+                                        --target_type project_flow_all \
+                                        --accum \
+                                        --accum_steps 16 \
+                                        --print_freq 100
+
+                                        # only for evaluation, add the following arguments
+                                        # --eval \
+                                        # --pretrained \
+                                        # --pretrained_ckpt ./save/icbhi_ast_ce_bs8_lr5e-5_ep50_seed1/best.pth
+
+    done
+done
